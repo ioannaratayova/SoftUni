4a74a538b601ec25e20ae80b355e17b5b4798cf0
Ioanna Ratayova <ioannaratayova@gmail.com>
2022-12-31 17:11:05 +0200

    JS Basics

---
+++ Basics/1. First Steps In Coding/6. Concatenate Data.js
@@ -0,0 +1,7 @@
+function ConcatenateDate([arg1, arg2, arg3, arg4])  {
+    let a = arg1;
+    let b = arg2;
+    let c = Number(arg3);
+    let d = arg4;
+    console.log(`You are ${a} ${b}, a ${c}-years old person from ${d}.`)
+}

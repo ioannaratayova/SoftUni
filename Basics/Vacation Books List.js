d4e3331b84d3e4915aaf9e57d14eb3e7536b39f7
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-01-05 21:56:28 +0200

    JS Basics

--- Basics/Vacation Books List.js
+++
@@ -1,8 +0,0 @@
-function VacationBookList([arg1, arg2, arg3])  {
-    let numberOfPages = Number(arg1);
-    let Pages_per_hour = Number(arg2);
-    let numberOfDaysForBook = Number(arg3);
-    let all_hours = numberOfPages / Pages_per_hour;
-    let hours_for_day = all_hours / numberOfDaysForBook;
-    console.log(hours_for_day);
-  }

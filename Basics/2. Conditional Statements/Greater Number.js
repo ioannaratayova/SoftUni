18cc56703132bece01288a619a07a333624ece59
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-01-10 17:02:22 +0200

    JS Basics

---
+++ Basics/2. Conditional Statements/Greater Number.js
@@ -0,0 +1,10 @@
+function GreaterNumber([arg1, arg2])  {
+    let num1 = Number(arg1);
+    let num2 = Number(arg2);
+    if (num1 > num2) {
+     console.log(num1);
+    }
+    else {
+     console.log(num2);
+    }
+ }

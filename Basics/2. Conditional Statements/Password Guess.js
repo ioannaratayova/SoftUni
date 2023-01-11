cfb9fbd9ae71bd78ed7cb70cfaef8f125116b642
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-01-11 13:20:55 +0200

    JS Basics

---
+++ Basics/2. Conditional Statements/Password Guess.js
@@ -0,0 +1,9 @@
+function PasswordGuess([arg1])  {
+    let n = arg1;
+    if (n == 's3cr3t!P@ssw0rd') {
+        console.log('Welcome');
+    }
+    else {
+        console.log('Wrong password!')
+    }
+}

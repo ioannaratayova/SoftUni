87cc44f65aea98fcc7fffa7988a12879ff56ff4c
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-01-31 13:51:29 +0200

    JS Basics

---
+++ Basics/4. For Loop/Vowels Sum.js
@@ -0,0 +1,20 @@
+function vowelsSum([n]) {
+    let sum = 0;
+    
+    for (let i = 0; i < n.length; i++) {
+       let currentLetter = n[i];
+       switch (currentLetter) {
+          case 'a': sum += 1;
+          break;
+          case 'e': sum += 2;
+          break;
+          case 'i': sum += 3;
+          break;
+          case 'o': sum += 4;
+          break;
+          case 'u': sum += 5;
+          break;
+       }
+    }
+    console.log(sum);
+}

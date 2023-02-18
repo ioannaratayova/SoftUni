4ebffce74502aaa9af58fe5cd602ce9b9cd0f9e3
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-02-18 08:48:49 +0200

    JS Basics

---
+++ Basics/6. Nested Loops/Multiplication Table.js
@@ -0,0 +1,8 @@
+function solve(args) {
+    for (let u = 1; u <= 10; u++) {
+     for (let m = 1; m <= 10; m++) {
+         let result = u * m;
+         console.log(`${u} * ${m} = ${result}`);
+     }
+    }
+}

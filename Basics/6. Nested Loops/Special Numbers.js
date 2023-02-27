c2ed2de002b5353ae87b101df82a65cd4f0094d8
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-02-27 10:02:18 +0200

    JS Basics

---
+++ Basics/6. Nested Loops/Special Numbers.js
@@ -0,0 +1,17 @@
+function solve(arg1) {
+    let num = Number(arg1);
+    let output = "";
+
+    for (let i = 1; i <= 9; i++) {
+        for (let j = 1; j <= 9; j++) {
+            for (let k = 1; k <= 9; k++) {
+                for (let l = 1; l <= 9; l++) {
+                    if (num % i == 0 && num % j == 0 && num % k == 0 && num % l == 0) {
+                            output += `${i}${j}${k}${l} `
+                    }
+                }
+            }
+        }
+    }
+    console.log(output)
+}

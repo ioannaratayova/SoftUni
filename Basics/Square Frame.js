aef1f0fb47532e8518941df150ec57a8295a80bb
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-03-05 09:59:24 +0200

    JS Basics

---
+++ Basics/Square Frame.js
@@ -0,0 +1,8 @@
+function SquareFrame([n]) {
+    n = Number(n);
+    console.log(`+ ${"- ".repeat(n - 2)}+`)
+    for (let row = 0; row < n - 2; row++) {
+        console.log(`| ${"- ".repeat(n - 2)}|`)
+    }
+    console.log(`+ ${"- ".repeat(n - 2)}+`)
+}

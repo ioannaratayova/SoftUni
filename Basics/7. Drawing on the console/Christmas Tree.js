7b8ed8b21ea5f402b2c583ac60f874c630e1aaf2
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-03-07 09:15:07 +0200

    JS Basics

---
+++ Basics/7. Drawing on the console/Christmas Tree.js
@@ -0,0 +1,8 @@
+function ChristmasTree([n]) {
+    n = Number(n);
+    console.log(" ".repeat(n + 1) + "|" );
+    for (let row = 0; row < n; row++) {
+        console.log(`${" ".repeat(n - 1 - row)}${'*'.repeat(row + 1)} | ${'*'.repeat(row + 1)}`);
+
+    }
+}

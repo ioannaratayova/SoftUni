55ee358e39af7f7e1ca85817b6d43ac81adefbc9
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-03-13 13:29:19 +0200

    JS Fundamentals

---
+++ Fundamentals/1. Basic Syntax, Conditional Statements and Loops/Student Information.js
@@ -0,0 +1,6 @@
+function studentInformation(arg1, arg2, arg3) {
+    let name= arg1;
+    let age = Number(arg2);
+    let grade = Number(arg3).toFixed(2);
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade}`);
+}

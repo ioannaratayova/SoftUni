e108287828dd09e5955849e8f681303e84b5eab8
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-03-14 14:37:44 +0200

    JS Fundamentals

---
+++ Fundamentals/1. Basic Syntax, Conditional Statements and Loops/Excellent grade.js
@@ -0,0 +1,9 @@
+function excellentGrade(arg1) {
+    let num = parseFloat(arg1);
+    if (num >= 5.50) {
+        console.log('Excellent');
+    }
+    else {
+        console.log('Not excellent')
+    }
+}

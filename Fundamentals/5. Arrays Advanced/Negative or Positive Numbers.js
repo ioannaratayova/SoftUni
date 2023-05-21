64016a0d42f8aa1ca77229a444f8b8faf440b279
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-05-21 19:47:43 +0300

    JS Fundamentals

---
+++ Fundamentals/5. Arrays Advanced/Negative or Positive Numbers.js
@@ -0,0 +1,13 @@
+function negativeOrPositiveNum(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] < 0) {
+            result.unshift(arr[i]);
+        }
+        else {
+            result.push(arr[i]);
+        }
+    }
+    console.log(result.join('\n'))
+}

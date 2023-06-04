b21f94626896a808128106e2601d5576c2807abb
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-06-04 19:37:35 +0300

    JS Fundamentals

---
+++ Fundamentals/6. Objects and Classes/Person Info.js
@@ -0,0 +1,7 @@
+function personInfo(firstName, lastName, age) {
+    let person = {};
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = age;
+    return person;  
+}

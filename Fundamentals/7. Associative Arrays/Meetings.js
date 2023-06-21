f37a3c87d5cc54eb2a18666eefb53e1238cad79d
Ioanna Ratayova <ioannaratayova@gmail.com>
2023-06-21 17:29:52 +0300

    JS Fundamentals

---
+++ Fundamentals/7. Associative Arrays/Meetings.js
@@ -0,0 +1,17 @@
+function meetings(arr) {
+    let meetings = {};
+    for (let line of arr) {
+        let [weekday, name] = line.split(" ");
+
+        if (meetings.hasOwnProperty(weekday)) {
+            console.log(`Conflict on ${weekday}!`);
+        }
+        else {
+            meetings[weekday] = name;
+            console.log(`Scheduled for ${weekday}`);
+        }
+    }
+    for (let meet in meetings) {
+        console.log(`${meet} -> ${meetings[meet]}`);
+    }
+}

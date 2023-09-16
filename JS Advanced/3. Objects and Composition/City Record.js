11d368cd7e227cc4e0e40046b3bedcc9e41c73a3
Ioanna Ratayova <117192715+ioannaratayova@users.noreply.github.com>
2023-09-16 22:19:32 +0300

    JS Advanced

---
+++ JS Advanced/3. Objects and Composition/City Record.js
@@ -0,0 +1,8 @@
+function solve(name1, population1, treasury1) {
+    let propertiesObj = {
+        name: name1,
+        population: population1,
+        treasury: treasury1
+    }
+    return propertiesObj;
+}

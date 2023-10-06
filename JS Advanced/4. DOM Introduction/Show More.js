70d7aa6cdf19fe877faad90fa6fbe902f8540189
Ioanna Ratayova <117192715+ioannaratayova@users.noreply.github.com>
2023-10-06 20:15:13 +0300

    JS Advanced

---
+++ JS Advanced/4. DOM Introduction/Show More.js
@@ -0,0 +1,6 @@
+function showText() {
+    let text = document.getElementById('more');
+    text.style.display = "none";
+    let text2 = document.getElementById('text');
+    text2.style.display = "inline";
+}
